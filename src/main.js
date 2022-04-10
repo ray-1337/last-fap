0dddaccc801f420d6591bd3d49b613ad6850a0a6
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-04-10 02:15:53 +0200

    use Eris.Client for help-typing purposes

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ let db = new jsoning(`./db/db.json`);
 
 dotenv.config();
 
-const bot = new Eris(String("Bot" + process.env["DISCORD_BOT_TOKEN"]), {
+const bot = new Eris.Client(String("Bot" + process.env["DISCORD_BOT_TOKEN"]), {
   intents: Eris.Constants.Intents.guilds | Eris.Constants.Intents.guildMessages,
   messageLimit: 0,
   restMode: true
