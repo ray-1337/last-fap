b2828f099dbf55f745143ae7e62616aa2961ae63
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-04-10 02:16:06 +0200

    use "client" instead of "bot"

--- src/main.js
+++ src/main.js
@@ -7,14 +7,14 @@ let db = new jsoning(`./db/db.json`);
 
 dotenv.config();
 
-const bot = new Eris.Client(String("Bot" + process.env["DISCORD_BOT_TOKEN"]), {
+const client = new Eris.Client(String("Bot" + process.env["DISCORD_BOT_TOKEN"]), {
   intents: Eris.Constants.Intents.guilds | Eris.Constants.Intents.guildMessages,
   messageLimit: 0,
   restMode: true
 });
 
-bot.on("ready", () => {
+client.on("ready", () => {
   console.log("Ready!");
 });
 
-bot.connect();
+client.connect();
