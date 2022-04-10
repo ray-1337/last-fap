564d2e1bedec5e95f1f4ebe88869cce0e91134a6
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-04-10 02:18:18 +0200

    added "pluris"

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const Eris = require("eris");
 const dotenv = require("dotenv");
 const jsoning = require("jsoning");
+const pluris = require("pluris");
 
 // initiate json base
 let db = new jsoning(`./db/db.json`);
@@ -11,6 +12,17 @@ const client = new Eris.Client(String("Bot" + process.env["DISCORD_BOT_TOKEN"]),
   intents: Eris.Constants.Intents.guilds | Eris.Constants.Intents.guildMessages,
   messageLimit: 0,
   restMode: true
+});
+
+pluris(Eris, {
+  awaitMessages: false,
+  awaitReactions: false,
+  createDMMessage: false,
+  embed: true,
+  endpoints: false,
+  messageGuild: false,
+  roleList: false,
+  webhooks: false
 });
 
 client.on("ready", () => {
